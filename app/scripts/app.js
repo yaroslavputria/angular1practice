826e803342de939c249873e021a6dcb93d2c18f9
Yaroslav Putria <yaroslav.putria@gmail.com>
2017-01-23 16:36:10 +0200

    empty yoeman angular project

---
+++ app/scripts/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name practiceApp
+ * @description
+ * # practiceApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('practiceApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true).hashPrefix('!');
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name practiceApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the practiceApp
+ */
+angular.module('practiceApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
